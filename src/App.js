777989edc33ae9557aa5cd55e7d78e45ca156c55
Carson Gotch <carsongotcha@gmail.com>
2021-04-13 17:31:08 -0600

    added material ui

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {withAuthenticator, AmplifySignOut} from '@aws-amplify/ui-react';
 import Amplify from '@aws-amplify/core';
+import { makeStyles } from '@material-ui/core';
+import API from '@aws-amplify/api';
+
+const ADMIN_USER = "cgotch@colostate.edu";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [admin, setAdmin] = useState(false);
+  
+  //my effects
+  useEffect(() => {
+    authorizeUser();
+  },[]);
+
+  //async functions
+  async function authorizeUser(){
+    const userInfo = await API.Auth.currentUserInfo();
+    setAdmin(userInfo.attributes.email === ADMIN_USER);
+  }
+
   return (
-    <div className="App">
-      <header>
-        <img src={logo} className="App-logo" alt="logo"/>
+    <div className="classes.root">
+      { admin === true && 
         <h1>
-          Now with Authentication
+          adminUI
         </h1>
-      </header>
-      <AmplifySignOut>
-      </AmplifySignOut>
+      }
+      { admin === false && 
+        <h1>
+          patientUI
+        </h1>
+      }
     </div>
   );
 }
